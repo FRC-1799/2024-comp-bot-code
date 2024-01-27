a1bc206ed5b74c5c7235cf16d1bb5268a459b5c5
ThePrinterFRC1799 <122649904+ThePrinterFRC1799@users.noreply.github.com>
2024-01-27 09:41:04 -0700

    much cleaner

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -36,9 +36,8 @@ public class Robot extends TimedRobot {
   final Bucket m_bucketSubsystem = new Bucket(pneumatics);
   final ToggleCompressor toggleCompressor = new ToggleCompressor(pneumatics);
   final Gyro gyro = new Gyro();
-  final Coords robotState=new Coords(); 
 
-  final Limelight lime = new Limelight(robotState);
+  final Limelight lime = new Limelight();
 
   final RunIntake runIntake = new RunIntake(m_intakeSubsystem, Constants.intake.fwdSpeed);
   final RunIntake runIntakeBackward = new RunIntake(m_intakeSubsystem, Constants.intake.revSpeed);
--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -3,49 +3,39 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import frc.robot.subsystems.Coords;
 
 public class Limelight extends SubsystemBase{
 boolean isBlue;
 NetworkTable table;
-Coords coordBox;
 //post to smart dashboard periodically
-    public Limelight(Coords coordBox){
-        this.coordBox=coordBox;
+    public Limelight(){
         isBlue=(DriverStation.getAlliance() == DriverStation.Alliance.Blue);
         table = NetworkTableInstance.getDefault().getTable("limelight");
     }
    @Override
    public void periodic(){
     
-    
-//read values periodically
-    
-    UpdateCoords(coordBox);
-    
-    SmartDashboard.putNumber("LimelightX", coordBox.getX());
-    SmartDashboard.putNumber("LimelightY", coordBox.getY());
-    SmartDashboard.putNumber("LimelightArea", coordBox.getZ());
 
     // SmartDashboard.putNumber("blueX", table.getEntry("botpose_wpilib."));
    }
-   public void UpdateCoords(Coords coords){
+   public Pose2d getlimeCoodrs(){
 
     if (table.getEntry("tid").getDouble(-1)==-1){
-        
+        return null;
     }
     else if(isBlue){
             
         double[] coordsList = table.getEntry("botpose_wpiblue").getDoubleArray(new double[6]);;
-        coords.setCoords(coordsList[0], coordsList[1], coordsList[2]); 
+        return new Pose2d(coordsList[0], coordsList[1], Rotation2d.fromDegrees(coordsList[2])); 
         
     }
     else{
         double[] coordsList = table.getEntry("botpose_wpired").getDoubleArray(new double[6]);;
-        coords.setCoords(coordsList[0], coordsList[1], coordsList[2]); 
-    }
+        return new Pose2d(coordsList[0], coordsList[1], Rotation2d.fromDegrees(coordsList[2]));    }
         
    }
 }
