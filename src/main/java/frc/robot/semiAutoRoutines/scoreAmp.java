1363205972ed22f66a8ddf8e833a578f228514d2
ThePrinterFRC1799 <122649904+ThePrinterFRC1799@users.noreply.github.com>
2024-02-03 10:22:22 -0700

    started adding routine

---
+++ src/main/java/frc/robot/semiAutoRoutines/scoreAmp.java
@@ -0,0 +1,9 @@
+package frc.robot.semiAutoRoutines;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class scoreAmp extends SequentialCommandGroup{
+    public scoreAmp(Blinkin LEDs, DriveBase drive, Intake){
+
+    }
+}
