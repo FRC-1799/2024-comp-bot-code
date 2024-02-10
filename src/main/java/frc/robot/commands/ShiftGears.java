1884830ddf235cc5185eac0c438c58064c92f817
ThePrinterFRC1799 <122649904+ThePrinterFRC1799@users.noreply.github.com>
2024-02-10 16:05:34 -0700

    finished

--- src/main/java/frc/robot/commands/ShiftGears.java
+++ src/main/java/frc/robot/commands/ShiftGears.java
@@ -1,12 +1,14 @@
 package frc.robot.commands;
 
+import java.util.function.BooleanSupplier;
+
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.subsystems.ShiftableGearbox;
 
-public class ShiftGears extends InstantCommand {
+public class ShiftGears extends RunCommand {
     
-    public ShiftGears(boolean isOn, ShiftableGearbox gearBox){
-        super(()->{gearBox.shift(isOn);}, gearBox);
-        addRequirements(gearBox);
+    public ShiftGears(ShiftableGearbox gearBox, BooleanSupplier isHigh){
+        super(()->{gearBox.shift(isHigh.getAsBoolean());}, gearBox);
     }
 }
