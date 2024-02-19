b89be23783fae511d6a368905b897e9108786225
Nova-Foxwood <chaffinsydney@gmail.com>
2024-02-19 11:39:01 -0700

    added the robot constants for where he needs to be

--- src/main/java/frc/robot/FeildConstants.java
+++ src/main/java/frc/robot/FeildConstants.java
@@ -1,25 +1,40 @@
 package frc.robot;
-
+ // all done in inches
 public class FeildConstants {
     public static final class Coords{
         public static final class NoteCoords{
-            public static final int X1 = 70;
-            public static final int Y1 = 2; 
-            public static final int X2 = 1;
-            public static final int Y2 = 42;
+            public static final double X1 = 70;
+            public static final double Y1 = 2; 
+            public static final double X2 = 1;
+            public static final double Y2 = 42;
         }
         public static final class SpeakerCoords{
-            public static final int X1 = 36;
-            public static final int Y1 = 125;
-            public static final int X2 = 36;
-            public static final int Y2 = 84;
+            public static final double X1 = 36;
+            public static final double Y1 = 125;
+            public static final double X2 = 36;
+            public static final double Y2 = 84;
         }
         public static final class AmpCoords{
-            public static final int X1 = 50;
-            public static final int Y1 = 0;
-            public static final int X2 = 95;
-            public static final int Y2 = 0;
+            public static final double X1 = 50;
+            public static final double Y1 = 0;
+            public static final double X2 = 95;
+            public static final double Y2 = 0;
         }
+
+    }
+    public static final class RobotCoordsRelation{
+            public static final class NoteRobotRelation{
+                public static final double X1 = 34.5;
+                public static final double Y1 = 54;
+            }
+            public static final class SpeakerRobotRelation{
+                public static final double X1 = 20.5;
+                public static final double Y1 = 51.75;
+            }
+            public static final class AmpRobotRelation{
+                public static final double X1 = 22.5;
+                public static final double Y1 = 15.75;
+            }
 
     }
 }
