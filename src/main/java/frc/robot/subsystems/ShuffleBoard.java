3a39b3030166fade67d855d1ba3c82e6953c60c8
Nova-Foxwood <chaffinsydney@gmail.com>
2024-02-19 10:21:33 -0700

    Work in Progress

---
+++ src/main/java/frc/robot/subsystems/ShuffleBoard.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.shuffleboard.SuppliedValueWidget;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardContainer;
+import frc.robot.subsystems.limitSwitch;
+
+public class ShuffleBoard {
+ public void ShuffleboardComponent​(ShuffleboardContainer parent, String Switch){
+   
+    public SuppliedValueWidget<Boolean> addBoolean ​(String Switch,
+    BooleanSupplier getVal) {
+        
+
+        }    
+    }
+}
