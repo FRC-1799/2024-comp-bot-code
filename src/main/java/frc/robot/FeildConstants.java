412a7ad3c85d57c6c85214ec9f40a6e811da9408
Nova-Foxwood <chaffinsydney@gmail.com>
2024-02-19 10:52:24 -0700

    Created Constants for the feild objects yay!!

---
+++ src/main/java/frc/robot/FeildConstants.java
@@ -0,0 +1,25 @@
+package frc.robot;
+
+public class FeildConstants {
+    public static final class Coords{
+        public static final class NoteCoords{
+            public static final int X1 = 70;
+            public static final int Y1 = 2; 
+            public static final int X2 = 1;
+            public static final int Y2 = 42;
+        }
+        public static final class SpeakerCoords{
+            public static final int X1 = 36;
+            public static final int Y1 = 125;
+            public static final int X2 = 36;
+            public static final int Y2 = 84;
+        }
+        public static final class AmpCoords{
+            public static final int X1 = 50;
+            public static final int Y1 = 0;
+            public static final int X2 = 95;
+            public static final int Y2 = 0;
+        }
+
+    }
+}
