b627e03ec1f41c40af895c9d43bda356f630475a
Nova-Foxwood <chaffinsydney@gmail.com>
2024-02-24 12:46:26 -0700

    Added Field for Computer

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,11 +10,13 @@ package frc.robot;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
 import frc.robot.subsystems.*;
 
 
@@ -33,6 +35,8 @@ public class Robot extends TimedRobot {
   final Elevator elevator = new Elevator();
 
   final Gyro gyro = new Gyro();
+
+  private Field2d field = new Field2d();
 
 
 
