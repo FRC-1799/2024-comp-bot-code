267b4c6f6fc959de3b20afba4eff2a6be5b0197c
ThePrinterFRC1799 <122649904+ThePrinterFRC1799@users.noreply.github.com>
2024-02-24 09:37:22 -0700

    maybe works:)

--- src/main/java/frc/robot/autoRoutines/ScoreAmp.java
+++ src/main/java/frc/robot/autoRoutines/ScoreAmp.java
@@ -1,11 +1,15 @@
 package frc.robot.autoRoutines;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.commands.DriveStraight;
 import frc.robot.commands.ElevatorToggle;
+import frc.robot.commands.RepetitiveIntake;
 import frc.robot.commands.RepetitiveOutake;
 import frc.robot.commands.TurnTo;
+import frc.robot.commands.WristMove;
 import frc.robot.subsystems.DriveBase;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Gyro;
@@ -16,9 +20,30 @@ public class scoreAmp extends SequentialCommandGroup{
     scoreAmp(DriveBase drive, Gyro gyro, Elevator elevator, WristIntake wrist, Intake intake){
         super(
             new DriveStraight(drive, 1.93294),
-            new TurnTo(drive, 90, gyro),
-            new ElevatorToggle(elevator),
-            new wristMove(wrist, Constants.auto.scoreAmp)
+
+            new TurnTo(drive, -90, gyro),
+
+            new ParallelCommandGroup(
+                new ElevatorToggle(elevator),
+                new WristMove(wrist, Constants.auto.scoreAmp)
+            ),
+            new RepetitiveOutake(intake),
+
+            new TurnTo(drive, 141.5, gyro),
+
+            new ParallelRaceGroup(
+                new DriveStraight(drive, 1.55),
+                new RepetitiveIntake(intake)
+            ),
+            new ParallelCommandGroup(
+                new TurnTo(drive, 180, gyro),
+                new ElevatorToggle(elevator)
+            ),
+
+            new DriveStraight(drive, 3),
+
+            new TurnTo(drive, 141.5, gyro),
+
             new RepetitiveOutake(intake)
         );
     }
